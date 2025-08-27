86dae3324beb2a3bcd4b7e816a7319562a027359
Bonheur2 <blissbonheur1@gmail.com>
2025-08-27 08:26:53 +0200

    graduation_processing

---
+++ src/app/registrar-academics/graduation-processing/page.tsx
@@ -0,0 +1,13 @@
+import { RegistrarLayout } from "@/components/registrar";
+import React from "react";
+
+export default function graduationProcessing() {
+    return (
+        <RegistrarLayout role="registrar-academics">
+        <main>
+            <h1>Graduation Processing</h1>
+            {/* Add graduation processing UI here */}
+        </main>
+        </RegistrarLayout>
+    );
+}
